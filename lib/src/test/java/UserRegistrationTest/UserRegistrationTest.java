bb9aa58bb9a0f855594342a144f7c143dab34056
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-15 13:05:23 +0530

    Added: Code to Enter a Vaild Mobile Number

--- lib/src/test/java/UserRegistrationTest/UserRegistrationTest.java
+++ lib/src/test/java/UserRegistrationTest/UserRegistrationTest.java
@@ -6,12 +6,12 @@ public class UserRegistrationTest
 {
 
     UserRegistration userRegistration = new UserRegistration();
-
+  
     @Test
-     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue()
+     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue() 
     {
-	 boolean result = userRegistration.validateFirstName("Nethaji");
-	 Assert.assertTrue(result);
+	  boolean result = userRegistration.validateFirstName("Nethaji");
+	  Assert.assertTrue(result);
 	}
     
      @Test
@@ -20,9 +20,9 @@ public class UserRegistrationTest
 	   boolean result = userRegistration.validateFirstName("Ne");
 	   Assert.assertFalse(result);
     }
-    
+   
      @Test
- 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse()
+ 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse() 
      {
  		boolean result = userRegistration.validateFirstName("Nethaji55");
  		Assert.assertFalse(result);
@@ -31,13 +31,65 @@ public class UserRegistrationTest
     @Test
     public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse()
     {
-	  boolean result = userRegistration.validateFirstName("Neth@ji");
-	  Assert.assertFalse(result);
+	 boolean result = userRegistration.validateFirstName("Nethaj!");
+	 Assert.assertFalse(result);
    }
     
     @Test
-    public void givenEmailId_WhenProper_ShouldReturnTrue(){
-        boolean result = userRegistration.validateEmailid("nethajichowdary555@gmail.com");
-        Assert.assertTrue(result);
-    }
-} 
+	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue()
+    {
+	    boolean result = userRegistration.validateLastName("Chowdary");
+	    Assert.assertTrue(result);
+	}
+	
+	@Test 
+	public void givenLastName_WhenLessThanThreeChar_shouldReturnFalse()
+	{
+		boolean result = userRegistration.validateLastName("Ch");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	  public void givenLastName_WhenHasNumber_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateLastName("Chowdary55");
+	      Assert.assertFalse(result);
+	  }
+
+	  @Test
+	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateLastName("Chowd@ry");
+	      Assert.assertFalse(result);
+	  }
+	  @Test
+	    public void givenEmailId_WhenProper_ShouldReturnTrue(){
+	        boolean result = userRegistration.validateEmailid("nethajich123@gmail.com");
+	        Assert.assertTrue(result);
+	    }
+	  
+	  @Test
+	    public void givenMobileNum_WhenSpaceIsMentioned_ShouldReturnTrue()
+	  {
+	        boolean result = userRegistration.validateMobileNum("91 7853736579");
+	        Assert.assertTrue(result);
+	    }
+	  
+	    @Test
+	    public void givenMobileNum_WhenSpaceNotMentioned_ShouldReturnFalse() {
+	        boolean result = userRegistration.validateMobileNum("9876543210");
+	        Assert.assertFalse(result);
+	    }
+	    @Test
+	    public void givenMobileNum_WhenLessDigits_ShouldReturnFalse() 
+	    {
+	        boolean result = userRegistration.validateMobileNum("99674");
+	        Assert.assertFalse(result);
+	    }
+	    @Test
+	    public void givenMobileNum_WhenNotStartWith91_ShouldReturnFalse()
+	    {
+	        boolean result = userRegistration.validateMobileNum("12345678");
+	        Assert.assertFalse(result);
+	    }
+
+
+}
