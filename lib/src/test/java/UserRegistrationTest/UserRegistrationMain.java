605f6d3c6e6a41bf559303b284935992d13a387e
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-15 15:00:20 +0530

    Added: Code to Check All The Valid Entries Using Exceptions

---
+++ lib/src/test/java/UserRegistrationTest/UserRegistrationMain.java
@@ -0,0 +1,15 @@
+package UserRegistrationTest;
+
+class UserRegistrationMain
+{
+     public static void main(String[] args) 
+	 {
+		UserRegistration myObj = new UserRegistration();
+		myObj.firstNameValidate();
+		myObj.lastNameValidate();
+		myObj.emailValidate();
+		myObj.mobileValidate();
+		myObj.passwordValidate();
+	 }
+
+}
