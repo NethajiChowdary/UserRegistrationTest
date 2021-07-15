c089c11d50cbf06d2c9912d4aebc764a815fdfc9
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-15 12:32:52 +0530

    Added: Code to Enter a Valid First Name

--- lib/src/test/java/UserRegistrationTest/UserRegistrationTest.java
+++ lib/src/test/java/UserRegistrationTest/UserRegistrationTest.java
@@ -1,5 +1,37 @@
 package UserRegistrationTest;
 
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
+public class UserRegistrationTest
+{
 
-}
+    UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue()
+    {
+	 boolean result = userRegistration.validateFirstName("Nethaji");
+	 Assert.assertTrue(result);
+	}
+    
+     @Test
+     public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse()
+     {
+	   boolean result = userRegistration.validateFirstName("Ne");
+	   Assert.assertFalse(result);
+    }
+    
+     @Test
+ 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse()
+     {
+ 		boolean result = userRegistration.validateFirstName("Nethaji55");
+ 		Assert.assertFalse(result);
+ 	}
+
+    @Test
+    public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse()
+    {
+	  boolean result = userRegistration.validateFirstName("Neth@ji");
+	  Assert.assertFalse(result);
+   }
+}   
